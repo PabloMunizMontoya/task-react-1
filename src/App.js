04ff1af02d928499508a18b4a7607d0fb8a08360
PabloMuñizMontoya <pablomunizmontoya@gmail.com>
2022-12-27 20:32:49 -0500

    creacion app react task 1

---
+++ src/App.js
@@ -0,0 +1,18 @@
+/* in this section all the items are rendered */
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>React task1</h1>
+        <p>
+          app de prueba 
+        </p>
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
